b5c7086a987890f559777a2b8b3519de4778f04c
faukiofficial <faukiofficial@gmail.com>
2024-05-03 17:50:36 +0700

    Menampahkan lorem100

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 <body>
     <h1>Hello world</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, voluptatibus.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio libero possimus iure eaque asperiores ipsam corporis, fugit quam aliquid laborum unde dolorem magnam facilis amet similique temporibus tempora officia impedit doloribus praesentium reprehenderit quia repellendus neque sint. Ex recusandae hic aliquid nihil accusamus praesentium quae deserunt totam expedita debitis autem id, numquam harum quibusdam delectus consequuntur nesciunt porro itaque quaerat iste quod doloremque accusantium dignissimos. Pariatur, inventore sed optio fugiat repellendus asperiores harum quasi accusamus assumenda soluta reprehenderit autem vero fuga voluptatum recusandae tenetur ipsum iste quam laborum doloremque aspernatur? Id veritatis accusantium porro quos ab commodi, nostrum obcaecati ipsa?</p>
 </body>
 </html>
